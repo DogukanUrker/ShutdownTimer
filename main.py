913c0478f3ac90c7de0bb133cf05b29e0bf8120a
Doğukan Ürker <62756402+DogukanUrker@users.noreply.github.com>
2021-05-14 21:13:44 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 def css(isim):
-    with open(isim,'r') as file:
-        icerik = file.read()
-        file.close()
+    with open(isim,'r') as dosya:
+        icerik = dosya.read()
+        dosya.close()
     return icerik
+
+
 class Pencere(QWidget):
     def __init__(self):
         super().__init__()
@@ -14,7 +16,15 @@ class Pencere(QWidget):
         self.setWindowTitle("Süreli PC Kapatma")
         self.setWindowIcon(QIcon('icons/bos.png'))
         self.setWindowFlags(Qt.WindowFlags(Qt.FramelessWindowHint))
+        self.dragPos = QPoint()
         self.ui()
+    def mousePressEvent(self, event):                                 
+        self.dragPos = event.globalPos()
+    def mouseMoveEvent(self, event):                                 
+        if event.buttons() == Qt.LeftButton:
+            self.move(self.pos() + event.globalPos() - self.dragPos)
+            self.dragPos = event.globalPos()
+            event.accept()       
     def ui(self):
         self.ok = QPushButton("Onayla",self)
         self.saat = QLineEdit("",self)
@@ -24,6 +34,10 @@ class Pencere(QWidget):
         self.exit = QPushButton("",self)
         self.minimized = QPushButton("",self)
         self.iptal = QPushButton("İptal Et",self)
+        self.kapat = QRadioButton("Kapat",self)
+        self.yenidenbaslat = QRadioButton("Restart",self)
+        self.kapat.setObjectName("kapat")
+        self.yenidenbaslat.setObjectName("yenidenbaslat")
         self.iptal.setObjectName("iptal")
         self.ok.setObjectName("onayla")
         self.exit.setObjectName("exit")
@@ -36,22 +50,29 @@ class Pencere(QWidget):
         self.saat.setGeometry(10,45,150,30)      
         self.yazi1.setGeometry(10,80,300,20)
         self.dakika.setGeometry(10,100,150,30)
+        self.yenidenbaslat.setGeometry(175,70,150,30)
+        self.kapat.setGeometry(175,40,150,30)
         self.ok.setGeometry(10,135,120,60)
         self.iptal.setGeometry(140,135,120,60)
         self.ok.clicked.connect(self.yap)
-        self.iptal.clicked.connect(self.kapa)
+        self.iptal.clicked.connect(self.islemiptal)
         self.exit.clicked.connect(QCoreApplication.instance().quit)
         self.minimized.clicked.connect(lambda: self.showMinimized())
     def yap(self):
+        def islem(komut,dk,saat):
+            if saat == " " or saat == "":
+                os.system("shutdown {} {}".format(komut,int(dk) * 60 ))   
+            elif dk == " " or dk == "":
+                os.system("shutdown {} {}".format(komut,int(saat) * 60 * 60 ))
+            else:
+                os.system("shutdown {} {}".format(komut,int(saat) * 60 * 60 + int(dk) * 60 ))
         saat = self.saat.text()
         dk = self.dakika.text()
         try:
-            if saat == " " or saat == "":
-                os.system("shutdown -s -f -t {}".format(int(dk) * 60 ))   
-            elif dk == " " or dk == "":
-                os.system("shutdown -s -f -t {}".format(int(saat) * 60 * 60 ))
-            else :
-                os.system("shutdown -s -f -t {}".format(int(saat) * 60 * 60 + int(dk) * 60 ))
+            if self.kapat.isChecked():
+                islem("-s -f -t",dk,saat)
+            if self.yenidenbaslat.isChecked():
+                islem("/r /t",dk,saat)
         except:
             msgBox = QMessageBox()
             msgBox.setText("Lütfen girdiğiniz değerleri kontrol edin. ")
@@ -59,7 +80,7 @@ class Pencere(QWidget):
             msgBox.setWindowFlags(Qt.WindowFlags(Qt.FramelessWindowHint))
             msgBox.setGeometry(600,250,100,100)
             ret = msgBox.exec_()            
-    def kapa(self):
+    def islemiptal(self):
         os.system("shutdown -a")
 app = QApplication(sys.argv)
 pencere= Pencere()
