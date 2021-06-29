c19f838e890bfa23b3a2f8ae68354fc33ff17f5c
Doğukan Ürker <62756402+DogukanUrker@users.noreply.github.com>
2021-06-29 18:25:40 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# SureliPCKapatma
-Basit bir pc kapatma zamanlayıcısı.
-.exe: https://mega.nz/folder/wN1hkKrA#Lbu3ga7-NJi_EoYdz-UYFA
+# ShutdownTimer
+Basic PC shutdown/restart app.
--- main.py
+++ main.py
@@ -3,25 +3,19 @@ from os import system
 from PyQt5.QtWidgets import QPushButton,QLineEdit,QLabel,QRadioButton,QMessageBox,QApplication,QWidget
 from PyQt5.QtCore import QPoint,QCoreApplication,Qt
 from PyQt5.QtGui import QIcon
-def css(isim):
-    with open(isim,'r') as dosya:
-        icerik = dosya.read()
-        dosya.close()
-    return icerik
-def uyarı(yazi):
-    mesaj = QMessageBox()
-    mesaj.setText(str(yazi))
-    mesaj.setStandardButtons(QMessageBox.Ok)
-    mesaj.setWindowFlags(Qt.WindowFlags(Qt.FramelessWindowHint))
-    mesaj.setGeometry(900,300,100,100)
-    ret = mesaj.exec_() 
-class Pencere(QWidget):
+def alert(text):
+    message = QMessageBox()
+    message.setText(str(text))
+    message.setStandardButtons(QMessageBox.Ok)
+    message.setWindowFlags(Qt.WindowFlags(Qt.FramelessWindowHint))
+    message.setGeometry(900,300,0,0)
+    ret = message.exec_() 
+class Window(QWidget):
     def __init__(self):
         super().__init__()
         self.setGeometry(600,400,270,200)
-        self.setWindowTitle("Süreli PC Kapatma")
-        self.setWindowIcon(QIcon('icons/bos.png'))
-        self.setWindowFlags(Qt.WindowFlags(Qt.FramelessWindowHint))
+        self.setWindowTitle("Shutdown Timer")
+        self.setWindowFlags(self.windowFlags() | Qt.FramelessWindowHint)
         self.dragPos = QPoint()
         self.ui()
     def mousePressEvent(self, event):                                 
@@ -32,80 +26,141 @@ class Pencere(QWidget):
             self.dragPos = event.globalPos()
             event.accept()       
     def ui(self):
-        self.ok = QPushButton("Onayla",self)
-        self.saat = QLineEdit("",self)
-        self.dakika = QLineEdit("",self)
-        self.yazi = QLabel("Saat:",self)
-        self.yazi1 = QLabel("Dakika:",self)
+        self.ok = QPushButton("Start",self)
+        self.hours = QLineEdit("",self)
+        self.minutes = QLineEdit("",self)
+        self.hours_text = QLabel("Hours:",self)
+        self.minutes_text = QLabel("Minutes:",self)
         self.exit = QPushButton("",self)
         self.minimized = QPushButton("",self)
-        self.iptal = QPushButton("İptal Et",self)
-        self.kapat = QRadioButton("Kapat",self)
-        self.yenidenbaslat = QRadioButton("Restart",self)
-        self.kapat.setObjectName("kapat")
-        self.yenidenbaslat.setObjectName("yenidenbaslat")
-        self.iptal.setObjectName("iptal")
-        self.ok.setObjectName("onayla")
+        self.abort = QPushButton("Abort",self)
+        self.shutdown = QRadioButton("Shutdown",self)
+        self.restart = QRadioButton("  Restart",self)
+        self.shutdown.setObjectName("shutdown")
+        self.restart.setObjectName("restart")
+        self.abort.setObjectName("abort")
+        self.ok.setObjectName("start")
         self.exit.setObjectName("exit")
         self.minimized.setObjectName("minimized")
-        self.exit.setIcon(QIcon('icons/exit.png'))
-        self.minimized.setIcon(QIcon('icons/minimized.png'))
-        self.exit.setGeometry(240,3,25,25)
-        self.minimized.setGeometry(215,3,25,25)
-        self.yazi.setGeometry(10,25,300,20)
-        self.saat.setGeometry(10,45,150,30)      
-        self.yazi1.setGeometry(10,80,300,20)
-        self.dakika.setGeometry(10,100,150,30)
-        self.yenidenbaslat.setGeometry(175,70,70,30)
-        self.kapat.setGeometry(175,40,70,30)
+        self.exit.setGeometry(245,3,20,20)
+        self.minimized.setGeometry(220,3,20,20)
+        self.hours.setGeometry(10,45,150,30)   
+        self.hours_text.setGeometry(10,25,300,20)
+        self.minutes.setGeometry(10,100,150,30)
+        self.minutes_text.setGeometry(10,80,300,20)
+        self.restart.setGeometry(175,70,85,30)
+        self.shutdown.setGeometry(175,40,85,30)
         self.ok.setGeometry(10,135,120,60)
-        self.iptal.setGeometry(140,135,120,60)
-        self.ok.clicked.connect(self.yap)
-        self.iptal.clicked.connect(self.islemiptal)
+        self.abort.setGeometry(140,135,120,60)
+        self.ok.clicked.connect(self.process)
+        self.abort.clicked.connect(self.shutdown_cancel)
         self.exit.clicked.connect(QCoreApplication.instance().quit)
         self.minimized.clicked.connect(lambda: self.showMinimized())
-        self.kapat.setChecked(True)
-    def yap(self):
-        kapat = "Bilgisayarınız {} dk içinde kapatılacaktır"
-        res = "Bilgisayarınız {} dk içinde yeniden başlatılacaktır"
-        def islem(komut,dk,saat):
-            if saat == " " or saat == "":
-                sure = int(dk) * 60 
-                system("shutdown {} {}".format(komut,sure))   
-                if komut == "-s -f -t":
-                    uyarı(kapat.format(dk))
-                else:
-                    uyarı(res.format(dk))
-            elif dk == " " or dk == "":
-                sure = int(saat) * 60 * 60 
-                sure2 = int(sure / 60 )
-                system("shutdown {} {}".format(komut,sure))
-                if komut == "-s -f -t":
-                    uyarı(kapat.format(sure2))
-                else:
-                    uyarı(res.format(sure2))
+        self.shutdown.setChecked(True)
+    def process(self):
+        hours = self.hours.text()
+        minutes = self.minutes.text()
+        def operation(command,minutes,hours):
+            if hours == " " or hours == "":
+                time = int(minutes) * 60 
+                system("shutdown {} {}".format(command,time))   
+            elif minutes == " " or minutes == "":
+                time = int(hours) * 60 * 60 
+                system("shutdown {} {}".format(command,time))
             else:
-                sure = int(saat) * 60 * 60 + int(dk) * 60 
-                sure2 = int(sure / 60 )
-                system("shutdown {} {}".format(komut,sure))
-                if komut == "-s -f -t":
-                    uyarı(kapat.format(sure2))
-                else:
-                    uyarı(res.format(sure2))
-        saat = self.saat.text()
-        dk = self.dakika.text()
+                time = int(hours) * 60 * 60 + int(minutes) * 60 
+                system("shutdown {} {}".format(command,time))
         try:
-            if self.kapat.isChecked():
-                islem("-s -f -t",dk,saat)
-            if self.yenidenbaslat.isChecked():
-                islem("/r /t",dk,saat)
+            if self.shutdown.isChecked():
+                operation("-s -f -t",minutes,hours)
+            if self.restart.isChecked():
+                operation("/r /t",minutes,hours)
         except:
-            uyarı("Lütfen Girdiğiniz değerleri kontrol edin")        
-    def islemiptal(self):
+            alert("Please check the values")        
+    def shutdown_cancel(self):
         system("shutdown -a")
-        uyarı("İşlem iptal edildi")
 app = QApplication(argv)
-pencere= Pencere()
-pencere.show()
-app.setStyleSheet(css("stylesheet.css"))
+Window= Window()
+Window.show()
+app.setStyleSheet(
+    """
+#start {
+  background: rgb(27, 167, 78);
+}
+#start:hover {
+  background: rgba(27, 167, 78,0.6);
+}
+#abort {  
+  background: rgba(255, 56, 56,0.8);
+}
+#abort:hover {
+  background: rgba(255, 56, 56,0.4);
+}
+#shutdown, #restart{
+    border-radius: 10px;
+    text-align: center;
+}
+#exit{
+    border-radius: 10px;
+  background: rgba(255, 56, 56,1.0);
+}
+#exit:hover{
+  background: rgba(255, 56, 56,0.7);
+}
+#minimized{
+    border-radius: 10px;
+  background: rgb(255, 159, 26);
+}
+#minimized:hover{
+  background: rgba(255, 159, 26,0.7);
+}
+QPushButton{
+  border-radius: 10px ;
+  font-size: 20px;
+  color: white;
+  font-weight: bold;
+}
+QWidget {
+  background: rgb(32, 30, 30);
+  color: aliceblue;
+}
+QLabel   {
+  font-size: 16px;
+  font-weight: bold;
+}
+QLineEdit {
+  border: 1px solid darkgrey;
+  border-radius: 7px;
+  padding: 1 6px;
+  font-size: 18px;
+}
+QMessageBox QLabel {
+    padding-right: 20px;
+  font-size:20px;
+  color: white;
+}
+QMessageBox QPushButton {
+    margin-right: 120px;
+    height: 30px;
+    width: 45px;
+  background: rgb(27, 167, 78);
+}
+QMessageBox QPushButton:hover {
+  background: rgba(27, 167, 78,0.6);
+}
+QRadioButton {
+  font-size: 16px;
+}
+QRadioButton:checked {
+  border: 1px solid rgb(101, 236, 101);
+  border-radius: 5px;
+}
+QRadioButton::indicator {
+  color : transparent;
+  background:transparent;
+  width: 1px;
+  height: 1px;
+}
+    """
+)
 app.exec()
